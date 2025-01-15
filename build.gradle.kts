8deed0031da2aaa7e3a4d9b96ba4693a3d470b78
lbachannel <anlb19749@gmail.com>
2025-01-15 21:59:12 +0700

    1st upload project

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("io.freefair.lombok") version "8.6"
+}
+
+group = "com.anlb"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/anlb/readcycle/ReadCycleApplication.java
@@ -0,0 +1,13 @@
+package com.anlb.readcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadCycleApplication.class, args);
+	}
+
+}
