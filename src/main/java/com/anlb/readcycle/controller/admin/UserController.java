e32f2b00753d2da5d3c51342c25fba85a2d448a4
lbachannel <anlb19749@gmail.com>
2025-01-16 14:42:35 +0700

    hash password

--- src/main/java/com/anlb/readcycle/controller/admin/UserController.java
+++ src/main/java/com/anlb/readcycle/controller/admin/UserController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,13 +19,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> createNewUser(@Valid @RequestBody RegisterDTO registerDTO) {
+        String hashPassword = this.passwordEncoder.encode(registerDTO.getPassword());
+        registerDTO.setPassword(hashPassword);
         User newUser = this.userService.registerDTOtoUser(registerDTO);
         this.userService.handleCreateUser(newUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
