7d7333d0325438f1495d796df35e8f976d702976
lbachannel <anlb19749@gmail.com>
2025-01-20 17:22:36 +0700

    create verify email token

--- src/main/java/com/anlb/readcycle/utils/SecurityUtil.java
+++ src/main/java/com/anlb/readcycle/utils/SecurityUtil.java
@@ -21,6 +21,23 @@ public class SecurityUtil {
         this.jwtEncoder = jwtEncoder;
     }
 
+    // verify email
+    @Value("${anlb.jwt.verify-email-token-validity-in-seconds}")
+    private long verifyEmailExpired;
+    public String createVerifyEmailToken(String email) {
+        Instant now = Instant.now();
+        Instant validity = now.plus(this.verifyEmailExpired, ChronoUnit.SECONDS);
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuedAt(now)
+                .expiresAt(validity)
+                .subject(email)
+                .claim("user", email)
+                .build();
+        JwsHeader jwsHeader = JwsHeader.with(JWT_ALGORITHM).build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).getTokenValue();
+    }
+
     @Value("${anlb.jwt.token-validity-in-seconds}")
     private long jwtExpired;
 
