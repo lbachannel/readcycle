4732c206b9cc5953608fced7c19479d27dc2e641
lbachannel <anlb19749@gmail.com>
2025-01-21 01:38:18 +0700

    asynchronous execution with api send email

--- src/main/java/com/anlb/readcycle/service/EmailService.java
+++ src/main/java/com/anlb/readcycle/service/EmailService.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -38,6 +39,7 @@ public class EmailService {
         }
     }
 
+    @Async
     public void sendEmailFromTemplateSync(User user, String subject, String templateName) {
         Context context = new Context();
         context.setVariable("email", user.getEmail());
