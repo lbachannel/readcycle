758654e8ed73c393d771015a57c0feb6669b7c68
lbachannel <anlb19749@gmail.com>
2025-01-24 16:45:53 +0700

    extends JpaSpecificationExecutor in repo

--- src/main/java/com/anlb/readcycle/repository/BookRepository.java
+++ src/main/java/com/anlb/readcycle/repository/BookRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.anlb.readcycle.domain.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     Optional<Book> findByIdAndIsActive(long id, boolean isActive);
 
     List<Book> findAllByIsActive(boolean isActive);
