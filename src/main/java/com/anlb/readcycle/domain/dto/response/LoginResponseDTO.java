79fa7e7db97b88d5c022e4191296f18dd615ac04
lbachannel <anlb19749@gmail.com>
2025-01-28 21:01:39 +0700

    format login response data

--- src/main/java/com/anlb/readcycle/domain/dto/response/LoginResponseDTO.java
+++ src/main/java/com/anlb/readcycle/domain/dto/response/LoginResponseDTO.java
@@ -1,11 +1,14 @@
 package com.anlb.readcycle.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 public class LoginResponseDTO {
+    @JsonProperty("access_token")
     private String accessToken;
     private UserLogin user;
 
