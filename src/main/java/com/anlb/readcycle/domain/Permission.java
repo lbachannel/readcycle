7ee8cfe5ebe80d0ff95c4e518a0c510d7fa52bdf
lbachannel <anlb19749@gmail.com>
2025-02-03 10:02:19 +0700

    update model permission

--- src/main/java/com/anlb/readcycle/domain/Permission.java
+++ src/main/java/com/anlb/readcycle/domain/Permission.java
@@ -1,8 +1,19 @@
 package com.anlb.readcycle.domain;
 
 import java.time.Instant;
+import java.util.List;
+
+import com.anlb.readcycle.utils.SecurityUtil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -10,6 +21,9 @@ import lombok.Data;
 @Table(name = "permissions")
 @Data
 public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String apiPath;
@@ -19,4 +33,22 @@ public class Permission {
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "permissions")
+    @JsonIgnore
+    private List<Role> roles;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                    ? SecurityUtil.getCurrentUserLogin().get() : "";
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                    ? SecurityUtil.getCurrentUserLogin().get() : "";
+        this.updatedAt = Instant.now();
+    }
 }
