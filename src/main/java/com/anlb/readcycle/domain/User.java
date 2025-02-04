945022ab457403c302ec232e64472e0a43d3140a
lbachannel <anlb19749@gmail.com>
2025-02-04 11:08:11 +0700

    update user with role

--- src/main/java/com/anlb/readcycle/domain/User.java
+++ src/main/java/com/anlb/readcycle/domain/User.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -28,4 +30,8 @@ public class User {
 
     @Column(columnDefinition = "MEDIUMTEXT")
     private String verificationEmailToken;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
