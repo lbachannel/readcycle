6a842162cf90850ceb1e96e934c1bd047212f809
lbachannel <anlb19749@gmail.com>
2025-02-11 15:07:46 +0700

    using jakarta.validation

--- src/main/java/com/anlb/readcycle/domain/dto/request/CreatePermissionRequestDTO.java
+++ src/main/java/com/anlb/readcycle/domain/dto/request/CreatePermissionRequestDTO.java
@@ -1,16 +1,18 @@
 package com.anlb.readcycle.domain.dto.request;
 
-import com.anlb.readcycle.utils.exception.PermissionChecked;
-
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@PermissionChecked
 public class CreatePermissionRequestDTO {
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Api path is required")
     private String apiPath;
+    @NotBlank(message = "Method path is required")
     private String method;
+    @NotBlank(message = "Module is required")
     private String module;
 }
