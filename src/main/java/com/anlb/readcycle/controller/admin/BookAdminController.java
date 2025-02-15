7f6702f9fcb6bd48664fc79fbcf6c5b89646427e
lbachannel <anlb19749@gmail.com>
2025-02-15 21:23:42 +0700

    remove import java.net.URISyntaxException

--- src/main/java/com/anlb/readcycle/controller/admin/BookAdminController.java
+++ src/main/java/com/anlb/readcycle/controller/admin/BookAdminController.java
@@ -1,7 +1,5 @@
 package com.anlb.readcycle.controller.admin;
 
-import java.net.URISyntaxException;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
