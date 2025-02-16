6caf8a467626ef4e0526f3962c1228f57bd1042d
lbachannel <anlb19749@gmail.com>
2025-02-16 11:57:21 +0700

    activity type

---
+++ src/main/java/com/anlb/readcycle/dto/activitylog/ActivityType.java
@@ -0,0 +1,30 @@
+package com.anlb.readcycle.dto.activitylog;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public enum ActivityType {
+    CREATE_USER("Create user"),
+    UPDATE_USER("Update user"),
+    DELETE_USER("Delete user"),
+
+    CREATE_BOOK("Create book"),
+    UPDATE_BOOK("Update book"),
+    DELETE_BOOK("Delete book");
+
+    @Getter
+    @Setter
+    private String name;
+
+    ActivityType(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return name;
+    }
+}
