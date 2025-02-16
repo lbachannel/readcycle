478a1be5f87ac5aaaeadc47bd7a424c21e89be10
lbachannel <anlb19749@gmail.com>
2025-02-16 12:11:49 +0700

    activity log

---
+++ src/main/java/com/anlb/readcycle/dto/activitylog/ActivityLog.java
@@ -0,0 +1,52 @@
+package com.anlb.readcycle.dto.activitylog;
+
+import java.time.Instant;
+import java.util.List;
+
+import com.anlb.readcycle.utils.JSON;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@Entity
+@Table(name = "activitylogs")
+public class ActivityLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "activity_type")
+    private String activityType;
+
+    @Column(name = "activity_group")
+    private String activityGroup;
+
+    @Column(name = "execution_time")
+    private Instant executionTime;
+
+    @Column(name = "description", columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    @Column(name = "username")
+    private String username;
+
+    public static ActivityLog formatLogMessage(ActivityGroup group, ActivityType type, List<ActivityDescription> descriptions) {
+        ActivityLog result = ActivityLog.builder()
+            .activityGroup(group.toString())
+            .activityType(type.toString())
+            .executionTime(Instant.now())
+            .description(JSON.toJson(descriptions))
+            .username("")
+            .build();
+        return result;
+    }
+}
