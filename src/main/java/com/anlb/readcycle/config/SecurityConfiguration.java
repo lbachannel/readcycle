6b73f1fa7231d00355173f66d4d9f03feafcabf5
lbachannel <anlb19749@gmail.com>
2025-02-19 09:00:52 +0700

    config /, /api/v1/books/** permitAll

--- src/main/java/com/anlb/readcycle/config/SecurityConfiguration.java
+++ src/main/java/com/anlb/readcycle/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
             .cors(Customizer.withDefaults())
             .authorizeHttpRequests(
                 authz -> authz
-                .requestMatchers("/api/v1/user/register", "/api/v1/auth/verify-email/**", "/api/v1/auth/login", "/api/v1/auth/refresh", "/upload/**").permitAll()
+                .requestMatchers("/", "/api/v1/books/**", "/api/v1/user/register", "/api/v1/auth/verify-email/**", "/api/v1/auth/login", "/api/v1/auth/refresh", "/upload/**").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
