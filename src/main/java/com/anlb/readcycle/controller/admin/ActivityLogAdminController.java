296118c54582345ca59ad8855cad41a9f4b72f98
lbachannel <anlb19749@gmail.com>
2025-02-26 16:00:33 +0700

    activity log controller

--- src/main/java/com/anlb/readcycle/controller/admin/ActivityLogAdminController.java
+++ src/main/java/com/anlb/readcycle/controller/admin/ActivityLogAdminController.java
@@ -1,18 +1,15 @@
 package com.anlb.readcycle.controller.admin;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.anlb.readcycle.dto.activitylog.ActivityLog;
 import com.anlb.readcycle.dto.response.ResultPaginateDto;
 import com.anlb.readcycle.service.IActivityLogService;
 import com.anlb.readcycle.utils.anotation.ApiMessage;
-import com.turkraft.springfilter.boot.Filter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,9 +29,9 @@ public class ActivityLogAdminController {
      */
     @GetMapping("/activity-log")
     @ApiMessage("Get all activity logs")
-    public ResponseEntity<ResultPaginateDto> getAllActivityLog(@Filter Specification<ActivityLog> spec, Pageable pageable) {
+    public ResponseEntity<ResultPaginateDto> getAllActivityLog(Pageable pageable) {
         return ResponseEntity
                     .status(HttpStatus.OK)
-                    .body(activityLogService.handleGetAllActivityLog(spec, pageable));
+                    .body(activityLogService.handleGetAllActivityLog(pageable));
     }
 }
