8bd9c0c09163e2d47bd7abe5bd9809961c6e5866
lbachannel <anlb19749@gmail.com>
2025-03-13 17:48:39 +0700

    config interceptor

--- src/main/java/com/anlb/readcycle/config/StaticResourcesWebConfiguration.java
+++ src/main/java/com/anlb/readcycle/config/StaticResourcesWebConfiguration.java
@@ -2,11 +2,17 @@ package com.anlb.readcycle.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class StaticResourcesWebConfiguration implements WebMvcConfigurer {
+
+    private final MaintenanceInterceptor maintenanceInterceptor;
 
     @Value("${anlb.upload-file.base-uri}")
     private String baseURI;
@@ -16,5 +22,13 @@ public class StaticResourcesWebConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations(baseURI);
     }
-}
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(maintenanceInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/admin/maintenance")
+                .excludePathPatterns("/api/v1/auth/login")
+                .excludePathPatterns("/api/v1/auth/logout");
+    }
+}
