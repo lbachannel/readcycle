d4c94348566d07473f06d2cf4d2b4e6fa3f34a27
lbachannel <anlb19749@gmail.com>
2025-03-14 15:05:38 +0700

    add java doc

--- src/main/java/com/anlb/readcycle/service/impl/CartServiceImpl.java
+++ src/main/java/com/anlb/readcycle/service/impl/CartServiceImpl.java
@@ -27,6 +27,17 @@ public class CartServiceImpl implements ICartService {
     private final CartRepository cartRepository;
     private final IBorrowBookService borrowBookService;
     
+    /**
+     * Adds a book to the user's cart.
+     *
+     * This method checks if the user has already borrowed the same book or a book of the same category
+     * before allowing them to add a new book to the cart. If any such condition is met, an exception is thrown.
+     *
+     * @param book the {@link Book} to be added to the cart.
+     * @return the newly created {@link Cart} entity.
+     * @throws InvalidException if the user is not authenticated, has already borrowed the same book, 
+     *         or has borrowed another book of the same category.
+     */
     @Override
     public Cart handleAddBookToCart(Book book) throws InvalidException {
         String email = SecurityUtil.getCurrentUserLogin()
@@ -56,6 +67,15 @@ public class CartServiceImpl implements ICartService {
         return newCart;
     }
 
+    /**
+     * Retrieves the list of carts associated with the currently authenticated user.
+     *
+     * This method fetches the authenticated user's information and returns all cart items belonging to them.
+     * If the user is not authenticated, an {@link InvalidException} is thrown.
+     *
+     * @return a list of {@link Cart} objects associated with the user.
+     * @throws InvalidException if the user is not authenticated (invalid access token).
+     */
     @Override
     public List<Cart> handleGetCartsByUser() throws InvalidException {
         String email = SecurityUtil.getCurrentUserLogin()
@@ -67,10 +87,26 @@ public class CartServiceImpl implements ICartService {
         return new ArrayList<>();
     }
 
+    /**
+     * Deletes a cart by its ID.
+     *
+     * This method removes the cart entry from the database based on the provided cart ID.
+     * If the cart does not exist, no action is taken.
+     *
+     * @param id the unique identifier of the cart to be deleted.
+     */
     public void handleDeleteCartById(long id) {
         cartRepository.deleteById(id);
     }
 
+    /**
+     * Deletes multiple carts by their IDs.
+     *
+     * This method removes all cart entries from the database that match the provided list of IDs.
+     * If any ID does not exist, no action is taken for that specific ID.
+     *
+     * @param ids the list of unique identifiers of the carts to be deleted.
+     */
     public void handleDeleteCarts(List<Long> ids) {
         cartRepository.deleteAllById(ids);
     }
