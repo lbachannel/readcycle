c0c47534a2d836def1a844298d1a5daf00e92890
lbachannel <anlb19749@gmail.com>
2025-03-14 15:19:22 +0700

    add java doc

--- src/main/java/com/anlb/readcycle/service/query/UserQueryService.java
+++ src/main/java/com/anlb/readcycle/service/query/UserQueryService.java
@@ -24,6 +24,16 @@ public class UserQueryService extends QueryService<User> {
 
     private final UserRepository userRepository;
 
+    /**
+     * Retrieves a paginated list of users based on the specified search criteria.
+     *
+     * This method constructs a {@link Specification} from the given {@link UserCriteria}
+     * and executes a query to fetch users matching the criteria in a paginated format.
+     *
+     * @param criteria the filtering criteria for retrieving users.
+     * @param pageable the pagination details.
+     * @return a {@link Page} containing the users that match the criteria.
+     */
     @Transactional(readOnly = true)
     public Page<User> findByCriteria(UserCriteria criteria, Pageable pageable) {
         log.debug("find by criteria : {}, page: {}", criteria, pageable);
@@ -31,6 +41,15 @@ public class UserQueryService extends QueryService<User> {
         return userRepository.findAll(specification, pageable);
     }
 
+    /**
+     * Builds a {@link Specification} for filtering users based on the given criteria.
+     *
+     * This method dynamically constructs query conditions based on the provided
+     * {@link UserCriteria}. It applies filters for name, email, date of birth, and role.
+     *
+     * @param criteria the filtering criteria containing conditions for querying users.
+     * @return a {@link Specification} representing the filtering conditions.
+     */
     protected Specification<User> createSpecification(UserCriteria criteria) {
         Specification<User> specification = Specification.where(null);
         if (criteria != null) {
